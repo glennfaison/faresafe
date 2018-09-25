8533f38cde2a4d37bc3e49e9d7b7121c6d78dd42
glennfaison <glennfaison@gmail.com>
2018-09-25 23:46:37 +0100

    Edited navbar

--- src/components/views/SubmissionSuccessView.jsx
+++ src/components/views/SubmissionSuccessView.jsx
@@ -4,17 +4,12 @@ import { withRouter } from 'react-router-dom';
 
 let SubmissionSuccessView = (props) => {
   return (
-    <section className="row">
+    <section className="row mb-5">
       {/* <div className="mt-4 pt-4 col-12"></div> */}
-      <div className="col-lg-7 col-md-6 order-last order-md-first">
+      <div className="col-lg-7 col-md-6 order-last order-md-first mt-1">
         <h1 className="text-bold p-4">You submitted successfully!</h1>
-        <p className="h4 p-4 text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Beatae exercitationem hic doloribus tenetur quia aliquam explicabo.
-          Voluptatibus qui in incidunt, animi vel soluta at deleniti
-          quas reiciendis ullam debitis ratione.
-        </p>
       </div>
-      <div className="col-lg-5 col-md-6 order-first order-md-last text-center">
+      <div className="col-lg-5 col-md-6 order-first order-md-last text-center mb-5">
         <i className="fa-check fas fa-10x fa-fw text-success"></i>
       </div>
     </section>
