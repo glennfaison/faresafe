c84dd8f1d4bf29584fff4dc4aa0f3928ef2a4037
glennfaison <glennfaison@gmail.com>
2018-09-25 21:35:16 +0100

    Edited navbar

--- src/components/views/WelcomeView.jsx
+++ src/components/views/WelcomeView.jsx
@@ -16,12 +16,6 @@ const WelcomeView = (props) => {
         <SignupForm hidden={(openFormName !== "signup")} hideThis={hideForm}/>
       </div>
       <div className="col-12 d-none order-lg-first ml-3 ml-lg-0 d-md-block col-lg-8">
-        <p className="h1 text-justify">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Perspiciatis voluptatibus dolor impedit recusandae illum sequi
-          quae, a, placeat aliquid itaque maxime mollitia consequuntur
-          delectus et molestias, corrupti ullam enim nihil.
-      </p>
       </div>
     </section>
   );
