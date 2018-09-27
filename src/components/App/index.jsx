11d0bfe91b2283f59292e19ad18708d8f9b7f877
glennfaison <glennfaison@gmail.com>
2018-09-27 14:28:48 +0100

    Added favicon images.

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -9,7 +9,7 @@ import PageTopAnchor from '../PageTopAnchor';
 // import ErrorView from '../views/ErrorView';
 import WelcomeView from '../views/WelcomeView';
 import ContactUsView from '../views/ContactUsView';
-import RedirectionView from '../views/RedirectionView';
+// import RedirectionView from '../views/RedirectionView';
 import FormSubmissionView from '../views/FormSubmissionView';
 import SubmissionSuccessView from '../views/SubmissionSuccessView';
 
@@ -36,10 +36,10 @@ class App extends React.Component {
                 <FormSubmissionView exact path={this.props.routes.submitForm} />
                 <SubmissionSuccessView exact path={this.props.routes.submissionSuccess} />
                 <AboutUsView exact path={this.props.routes.aboutUs} />
-                {/* <ErrorView exact={true} path={this.props.routes.error} /> */}
+                {/* <ErrorView exact={true} path={this.props.routes.error} />
                 <RedirectionView exact={false}
                   path={this.props.routes.root}
-                  redirectPath={this.props.routes.welcome} />
+                  redirectPath={this.props.routes.welcome} /> */}
               </Switch>
             </section>
           </div>
